381db373fb3384ea0cae26edff5131fcb0edc8eb
krozic <kyle.rozic@gmail.com>
2023-01-17 01:06:20 -0500

    render migration

---
+++ table_to_json.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import json
+
+table = pd.read_csv('./imdb_genre_rank.csv')
+
+genres = list(table.columns[1:])
+ratings = table['rating']
+
+genre_dict = {}
+
+for genre in genres:
+    genre_dict[genre] = {}
+    for i in range(0, len(ratings)):
+        rating = ratings[i]
+        rank = table[genre][i]
+        if not np.isnan(rank):
+            genre_dict[genre][rating] = rank
+
+# json_pretty = json.dumps(genre_dict, indent=4)
+
+with open('imdb_genre_rank.json', 'w') as outfile:
+    json.dump(genre_dict, outfile, indent=4)
