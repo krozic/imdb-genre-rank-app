957f8455b23fa793ed4d053ff85903d0b74ad264
krozic <kyle.rozic@gmail.com>
2023-12-09 23:04:59 -0500

    changed to my own hosted api search

--- imdb_search.py
+++ imdb_search.py
@@ -1,10 +1,11 @@
 import requests
-from my_secrets import imdb_api
+# from my_secrets import imdb_api
 from typing import List, Dict
 
 def imdb_search(query: str) -> List[Dict[str, str]]:
     query.replace(' ', '%20')
-    r = requests.get(f'https://imdb-api.com/en/API/Search/{imdb_api}/{query}')
+    # r = requests.get(f'https://imdb-api.com/en/API/Search/{imdb_api}/{query}')
+    r = requests.get(f'https://query-lookup.kyle-rozic.workers.dev/search?query={query}')
     data = r.json()
     options = []
     i = 0
@@ -12,6 +13,11 @@ def imdb_search(query: str) -> List[Dict[str, str]]:
         if i > 4:
             break
 
-        options.append({"label": f'{title["title"]} {title["description"][:4]}', "value": title["id"]})
-        i+=1
+        if 'year' in title.keys():
+            # options.append({"label": f'{title["title"]} {title["description"][:4]}', "value": title["id"]})
+            options.append({"label": f'{title["title"]} {title["year"]}', "value": title["id"]})
+            i+=1
     return options
+
+# results = imdb_search("Inception")
+# print(results)
